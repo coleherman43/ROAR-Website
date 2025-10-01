faea6781a707c1fafd268d67a7fb990800b3bd6e
coleherman43 <colesten@gmail.com>
2025-10-01 15:41:47 -0700

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Important for static hosting
+  base: '/ROAR-Website/',
   build: {
-    outDir: 'dist',
+    outDir: 'dist'
   }
 })
