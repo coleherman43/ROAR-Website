21f3052dd212b79117b9c205460da4d8f4c21e9c
coleherman43 <colesten@gmail.com>
2025-10-01 16:28:08 -0700

    Changed deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/ROAR-Website/',
-  build: {
-    outDir: 'dist'
+export default defineConfig(({ command, mode }) => {
+  const base = mode === 'production' ? '/ROAR-Website/' : '/'
+  
+  return {
+    plugins: [react()],
+    base: base,
+    build: {
+      outDir: 'dist'
+    }
   }
 })
